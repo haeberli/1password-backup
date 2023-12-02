86950331cfe0b7c6c288cbd5587479bea6c90d58
Christian Haeberli <christian.haeberli@gmx.net>
2023-12-02 02:13:51 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,13 +2,22 @@
 
 echo "1Password Backup"
 
+echo "- sign in"
 eval $((echo $PASSWORD echo -e "\n") | op signin my.1password.com $EMAIL $SECRETKEY)
+echo "  succeeded"
 
-items = $(op item list)
+echo "- list items"
+items = $(op list item)
+echo "  succeeded"
 
 for item in $items
+    echo "- get item"
     content = $(op get item ${item})
+    echo "  succeeded"
+    
     echo $content
 done
 
+echo "- sign out"
 op signout
+echo "  succeeded"
