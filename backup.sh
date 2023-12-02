ba1652a706d8030483601b4acdad882197eb26fb
Christian Haeberli <christian.haeberli@gmx.net>
2023-12-03 00:32:06 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,21 +3,20 @@
 echo "1Password Backup"
 
 echo "- sign in"
-eval $((echo $PASSWORD echo -e "\n") | op signin my.1password.com $EMAIL $SECRETKEY)
-echo "  done"
+eval $(echo $PASSWORD | op account add --address my.1password.com --email $EMAIL --secret-key $SECRET --signin)
+echo ". done"
 
 echo "- list items"
-items = $(op list)
-echo "  done"
+items=$(op item list --format=json | jq -r ".[].id")
+echo ". done"
 
 for item in $items
-    echo "- get item"
-    content = $(op item get ${item})
-    echo "  done"
-    
-    echo $content
+do
+  echo "- get item" 
+  op item get $item --format=json
+  echo ". done"
 done
 
 echo "- sign out"
 op signout
-echo "  done"
+echo ". done"
