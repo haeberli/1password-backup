691be522fca5b19aee61127ffa9b69dcb3af7c86
Christian Haeberli <christian.haeberli@gmx.net>
2023-12-04 00:20:05 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,36 +1,82 @@
 #!/bin/bash
 
-echo "1Password Backup"
+check () {
+  unset EMAIL
+  unset SECRETKEY
+  unset PASSWORD
 
-echo "- sign in"
-eval $(echo $PASSWORD | op account add --address my.1password.com --email $EMAIL --secret-key $SECRETKEY --signin)
-echo ". done"
+  chmod 700 $1
+  . "$1"
 
-echo "- list items"
-items=$(op item list --format=json | jq -r ".[].id")
-echo ". done"
+  if [ -z "$EMAIL" ]; then
+    echo "EMAIL not specified"
+    return
+  fi
 
-i=1
-for item in $items
-do
-  echo "- get $i: $item" 
-  get=$(op item get $item --format=json)
-  echo ". done"
+  if [ -z "$SECRETKEY" ]; then
+    echo "SECRETKEY not specified"
+    return
+  fi
 
-  title=$(echo $get | jq -r ".title" | sed -e "s/[\\/:\*\?\"<>\|\x01-\x1F\x7F]/_/g")
-  vault=$(echo $get | jq -r ".vault.name" | sed -e "s/[\\/:\*\?\"<>\|\x01-\x1F\x7F]/_/g")
-  content="$(echo $get | jq ".")" 
+  if [ -z "$PASSWORD" ]; then
+    echo "PASSWORD not specified"
+    return
+  fi
 
-  echo "  Title: $title, Vault: $vault"
+  echo "Check $EMAIL"
 
-  dir="./vaults/$EMAIL/$vault"
+  eval $(echo $PASSWORD | op account add --address my.1password.com --email $EMAIL --secret-key $SECRETKEY --signin)
 
-  mkdir -p "$dir"
-  echo "$content" > "$dir/$title-$item.json"
+  echo "List items"
+  local ids=$(op item list --format=json | jq -r ".[].id")
 
-  ((i++))
+  local count=0
+  local written=0
+
+  for id in $ids
+  do
+    echo "Get $i: $id" 
+    local item="$(op item get $id --format=json | jq .)"
+    ((count++))
+
+    local title=$(echo $item| jq -r ".title" | sed -e "s/[\\/:\*\?\"<>\|\x01-\x1F\x7F]/_/g")
+    local vault=$(echo $item| jq -r ".vault.name" | sed -e "s/[\\/:\*\?\"<>\|\x01-\x1F\x7F]/_/g")
+
+    if [ -z "$title" ]; then
+      echo "Empty title > skip"
+      continue
+    fi
+
+    local dir="./vaults/$EMAIL/$vault"
+    local file="$dir/$title $id.json"
+
+    local exist=""
+    if [ -f "$file" ]; then
+      exist=$(cat "$file")
+    fi
+
+    if [[ "$item" == "$exist" ]]; then
+      continue
+    fi
+
+    echo "Write $vault/$title"
+    mkdir -p "$dir"
+    echo "$item" > "$file"
+
+    ((written++))
+  done
+
+  echo "Sign out"
+  op signout
+
+  echo "Check $EMAIL finished: $count items, $written written"
+}
+
+while true; do
+  for account in vaults/*.conf; do
+    check $account
+  done
+
+  echo "Sleep 1 hour"
+  sleep 3600
 done
-
-echo "- sign out"
-op signout
-echo ". done"
